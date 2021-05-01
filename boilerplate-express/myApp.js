51dfc8797926ebf571dfa6c8e735ba431681ef08
Serhii <malgothsm@gmail.com>
2021-05-01 18:32:18 +0300

    Solved

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get("/", (req, res) => {
-  res.send('Hello Express');
-});
+//  app.get("/", (req, res) => {
+//   res.send('Hello Express');
+// });
+
+
 
 let absolutePath = __dirname + "/views/index.html";
 app.get("/", function(req, res) {
@@ -12,59 +14,22 @@ app.get("/", function(req, res) {
 
 app.use("/public",  express.static(__dirname + "/public"));
 
-app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json"
-  });
-});
+// app.get("/json", (req, res) => {
+//   res.json({
+//     message: "Hello json"
+//   });
+// });
 
-///Can't do it
+
+
 let messageS = "Hello json";
 
 app.use('/json', (req, res) => {
 
-  if (mySecret === "uppercase") {
+  if (process.env['MESSAGE_STYLE'] === "uppercase") {
     return res.json({ "message": messageS.toUpperCase() })
   }
   else {
     return res.json({ "message": messageS })
   }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
