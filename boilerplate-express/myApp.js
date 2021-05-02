19766c8c6172fec749eb69860f1fca14f47f586d
Serhii <malgothsm@gmail.com>
2021-05-02 15:17:44 +0300

    Made 10 ex

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var app = express();
 
+//// №2
 //  app.get("/", (req, res) => {
 //   res.send('Hello Express');
 // });
 
 
-
+//// №3
 let absolutePath = __dirname + "/views/index.html";
 app.get("/", function(req, res) {
   res.sendFile(absolutePath);
 });
-
+//// №4
 app.use("/public",  express.static(__dirname + "/public"));
 
+//// №5
 // app.get("/json", (req, res) => {
 //   res.json({
 //     message: "Hello json"
@@ -21,7 +23,7 @@ app.use("/public",  express.static(__dirname + "/public"));
 // });
 
 
-
+//// №6
 let messageS = "Hello json";
 
 app.use('/json', (req, res) => {
@@ -33,3 +35,40 @@ app.use('/json', (req, res) => {
     return res.json({ "message": messageS })
   }
 })
+
+//// №7
+app.use(function middleware(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
+//// №8
+
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();  
+  next();
+},
+(req, res) => {
+  res.send({
+    time: req.time
+  });
+}
+);
+
+
+//// №9
+app.get('/:word/echo', (req, res, next) => {
+  const {word} = req.params;
+ res.json({
+    echo: word
+  });
+});
+
+
+
+//// №10
+app.get('/name', (req, res) => {
+  let { first: firstName, last: lastName } = req.query;
+ res.json({name: `${firstName} ${lastName}`});
+});
+
