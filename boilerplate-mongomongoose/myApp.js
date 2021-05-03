3f0d94b774eafcdad43776fc45afa9a73c13f147
Serhii <malgothsm@gmail.com>
2021-05-03 17:34:24 +0300

    Finished the first

--- boilerplate-mongomongoose/myApp.js
+++ boilerplate-mongomongoose/myApp.js
@@ -1,13 +1,10 @@
 require('dotenv').config();
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://Serhii:<password>@freecodecamp.tglte.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const mongoose = require('mongoose');
+let uri = process.env.MONGO_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 let Person;
 
