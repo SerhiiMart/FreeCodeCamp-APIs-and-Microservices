1bb2823087e3e00e505e6e974553159898b2051f
Serhii <malgothsm@gmail.com>
2021-05-08 18:32:28 +0300

    App is probably working. Testing now

--- boilerplate-project-exercisetracker/server.js
+++ boilerplate-project-exercisetracker/server.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser')
 const uri = process.env.MONGO_URI;
 const mongoose = require('mongoose');
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-const bodyParser = require('body-parser');
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 5000 }, (error) => {
+  if (error) console.log(error);
+  console.log("Connection to the Database is successful");
+});
+
 
 app.use(cors())
 app.use(express.static('public'))
@@ -13,32 +17,121 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+////Working on exers
 
-let exerciseTrackerSchema = new mongoose.Schema({
-  description: {type : String, required: true,  unique: true,},
+let DDDSchema = new mongoose.Schema({
+  description: {type : String, required: true},
   duration: {type : Number, required: true},
   date: String
 })
 
 let userSchema = new mongoose.Schema({
-  username: {type: String, required: true},
-  log: [exerciseTrackerSchema]
+  username: {type: String, required: true, unique: true},
+  log: [DDDSchema]
 })
 
-let Session = mongoose.model("Session", exerciseTrackerSchema);
+let Log = mongoose.model("Log", DDDSchema);
 let User = mongoose.model("User", userSchema);
 
-app.post('/api/users', bodyParser.urlencoded({ extended: false }), (req, res) => {
-  let newUser = new User({username: req.body.username})
-  newUser.save((error, savedUser) => {
-    if(!error){
-      let resObj = {}
-      resObj['username'] = savedUser.username
-      resObj['_id'] = savedUser.id
-      res.json(resObj)
+app.post("/api/users", (req, res) => {
+  const { username } = req.body;
+
+   User.findOne({ username }, (err, found)=>{
+    if(err) return res.send(err);
+    if(found) res.send("Username already taken");
+    else {
+      const newUser = new User({ username });
+
+       newUser.save(async (err, user) => {
+        if(err) res.send(err);
+        res.json({username: newUser.username, _id: newUser.id})
+      });     
     }
-  })
+  });
+});
+
+app.get("/api/users", (req, res) => {
+  User.find().select("id username __v").exec((err, data) => {
+    res.json(data)
+  });
+});
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const { description, duration, date } = req.body;
+  const parsedDuration = Number(duration);
+
+  let newDate;
+
+  if(!date){
+    newDate = new Date();
+  } else if(!new Date(date)) {
+    return res.send("invalid date format");
+  } else{
+    newDate = new Date(date);
+  }
+
+  const newLog = new Log({
+    description,
+    duration: parsedDuration,
+    date: newDate
+    });
+
+  User.findByIdAndUpdate(
+    req.params["_id"],
+    {$push:{log: newLog}},
+    {new: true},
+    (err, found) => {
+    if(err) return res.json(err);
+    console.log(newDate);
+    res.json({
+      _id: found.id,
+      username: found.username,
+      date: newDate.toDateString(),
+      duration: parsedDuration,
+      description
+      });
+  });
+});
+
+
+app.get("/api/users/:_id/logs", (req, res) => {
+  const { _id } = req.params;
+  let { from, to, limit } = req.query;
+
+  User.findById(_id).select("-log._id").exec((err, user) => { 
+    if(err) res.send(err);
+
+    if(from || to){
+      to = (to === undefined) ? to : new Date(to);
+      from = (from === undefined) ? from : new Date(from);
+      user.log = user.log.filter(i => to ? i.date <= to : true && from ? i.date >= from : true);
+    }
+
+    if(limit){
+      user.log = user.log.slice(0,limit);
+    }
+
+    res.json({
+      _id: _id,
+      username: user.username,
+      from: from ? from.toDateString() : from,
+      to: to ? to.toDateString() : to,
+      count: user.log.length,
+      log: user.log.map(i => {
+        return {
+          description: i.description,
+          duration: i.duration,
+          date: i.date.toDateString()
+        }
+      })
+    })
+  });
+});
+
+
+
+////Listener
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
 })
