e8d2515d44443ae6b0b00e88b7387e04048c4fc7
Serhii <malgothsm@gmail.com>
2021-05-08 12:46:00 +0300

    Created Schemas

--- boilerplate-project-exercisetracker/server.js
+++ boilerplate-project-exercisetracker/server.js
@@ -16,3 +16,17 @@ app.get('/', (req, res) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+let exerciseTrackerSchema = new mongoose.Schema({
+  description: {type : String, required: true},
+  duration: {type : Number, required: true},
+  date: String
+})
+
+let userSchema = new mongoose.Schema({
+  username: {type: String, required: true},
+  log: [exerciseTrackerSchema]
+})
+
+let Session = mongoose.model("Session", exerciseTrackerSchema);
+let User = mongoose.model("User", userSchema);
