5de6a7541226803b9759cf3d4d4a14b2d67dd85a
Serhii <malgothsm@gmail.com>
2021-05-08 11:23:08 +0300

    Added mangodb

--- boilerplate-project-exercisetracker/server.js
+++ boilerplate-project-exercisetracker/server.js
@@ -4,16 +4,13 @@ const cors = require('cors')
 require('dotenv').config()
 const uri = process.env.MONGO_URI;
 const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-
-
-
-
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
