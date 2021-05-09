4446ef8e1b17d1d1b3e0a77f9873eb43bbb0a7aa
Serhii <malgothsm@gmail.com>
2021-05-09 15:07:36 +0300

    Finished the fifth ex

--- boilerplate-project-filemetadata/server.js
+++ boilerplate-project-filemetadata/server.js
@@ -11,10 +11,19 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+   const output = {
+  'name': req.file.originalname,
+  'type' : req.file.mimetype,
+  'size' : req.file.size
+}
+  next().res.json(output)
+})
+
